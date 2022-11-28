3bfd6fe07a76dbf300785514c3560d1217c3542c
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2022-11-28 12:23:52 -0800

    11-28

--- MidtermReview/Midterm.js
+++ MidtermReview/Midterm.js
@@ -14,10 +14,14 @@ let boxSpeed;
 //fields for number6
 let submitNumber6;
 let displayNumber6;
+//This is the array of cars
 let carList;
 
 //field for number 7
 let selectedCar;
+let inputNumber7;
+let submitNumber7;
+
 
 
 //use the load event like a constructor
@@ -36,9 +40,61 @@ window.addEventListener('load', function(){
     loadCars();
     submitNumber6.addEventListener('click', displayCars);
     //number 7
-    displayNumber6.addEventListener('click', test);
+    displayNumber6.addEventListener('click', getSelectedCar);
+    inputNumber7 = document.getElementById('inputNumber7');
+    submitNumber7 = document.getElementById('submitNumber7');
+    //add a listener to submit button for number 7
+    submitNumber7.addEventListener('click', updateCar);
+    //references for #1
+    inputNumber1 = document.getElementById('inputNumber1');
+    submitNumber1 = document.getElementById('submitNumber1');
+    displayNumber1 = document.getElementById('displayNumber1');
+    submitNumber1.addEventListener('click', displayPrime);
+    
 
 });
+function displayPrime(){
+    let number = inputNumber1.value;
+    if(!number){
+        alert('Please enter a number');
+        return;
+    }
+    //we need a list of the factors... if that list is empty, then I know the number is prime
+    let factors = getFactors(number);
+    // console.log(factors);
+    if(factors.length === 0){
+        displayNumber1.innerHTML = `${number} is Prime`;
+    }
+    //if there are factors the number is not prime
+    else{
+        displayNumber1.textContent = `${number} is not prime. It is divisible by: `;
+        factors.forEach(x => {
+            displayNumber1.textContent += `${x} `;
+        })
+    }
+}
+
+//method that takes a number and returns the factors
+function getFactors(number){
+    let result = [];
+    if(number == 2 || number == 3){
+        return result;
+    }
+    // if(number % 2 === 0){
+    //     //the number is not prime, it is even
+    //     result.push(2);
+    //     result.push(number / 2);
+    // }
+    //now we are ready
+    for(let i = 2; i <= Math.sqrt(number); i++){
+        if(number % i == 0){
+            result.push(i);
+            result.push(number / i);
+        }
+    }
+    //if we never added any numbers to the array we know that number is prime
+    return result;
+}
 
 //Let's create moveBox 
 //this moveBox is going to take an event
@@ -119,19 +175,49 @@ function displayCars(){
         displayNumber6.appendChild(element);
     }
 }
+//This function will take and event and then
+//set the selectedCar variable to whatever car
+//was click on in the list
+function getSelectedCar(event){
+    //Created an array from all of the list item elements
+    let listItemArray = Array.from(displayNumber6.children);
+    //Now I can use the indexOf()
+    let clickedIndex = listItemArray.indexOf(event.target);
+    //now we have the index
+    //use the index to access the car
+    selectedCar = carList[clickedIndex];
+    //call the displayOneCar method and pass the selectedCar
+    displayOneCar(selectedCar);
+}
 
-function test(event){
-    //We need the index of the list item that got clicked
-    //If we have the index of the list item that got clicked
-    //we have the index of the corresponding car in the array
-    console.log(event.target);
-    //if we have the list item could you find what index it is at
-    //in the unordered list?
-    //you are looking for the index of what got clicked on
-    //There is an array indexOf method [10, 2, 5, 20]
-    //let index = numbers.indexOf(20);
+//lets make a method that takes a car and displays it into the
+//inputs for #7
+function displayOneCar(car){
+    if(!car){
+        return;
+    }
+    //get a reference to all of the inputs for number 7
+    let carInputs = inputNumber7.querySelectorAll('input');
+    // console.log(selectedCar);
+    // console.log(carInputs);
+    carInputs[0].value = selectedCar.year;
+    carInputs[1].value = selectedCar.make;
+    carInputs[2].value = selectedCar.model;
+    carInputs[3].value = selectedCar.milage;
+    carInputs[4].value = selectedCar.price;
+}
 
-    selectedCar = carList[selectedIndex];
-    //display the selected car
-
+function updateCar(){
+    //if no car is selected
+    if(!selectedCar){
+        return;
+    }
+    let carInputs = inputNumber7.querySelectorAll('input');
+    selectedCar.year = carInputs[0].value;
+    selectedCar.make = carInputs[1].value;
+    selectedCar.model = carInputs[2].value;
+    selectedCar.milage = carInputs[3].value;
+    selectedCar.price = carInputs[4].value;
+    //call my method that displays the cars
+    displayCars();
 }
